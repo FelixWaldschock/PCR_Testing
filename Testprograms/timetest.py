ba92a21292bbdf4433ba5247b8d53a6a44a5a090
SebastianBinggeli <104633452+SebastianBinggeli@users.noreply.github.com>
2022-05-13 13:34:14 +0200

    Update timetest.py

--- Testprograms/timetest.py
+++ Testprograms/timetest.py
@@ -1,4 +1,15 @@
 import time
+from ads1015 import ADS1015
+import RPi.GPIO as GPIO
+
+import sys
+import os
+#import from parent dir
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
 import sensor
 
 # create sensor objects
@@ -6,6 +17,26 @@ Temperature1 = sensor.Sensor("PT1000", ['in0/in1'],3.3, 20)
 Photodiode1 = sensor.Sensor("Photodiode", ['in2/gnd'],3.3, 20)
 Photodiode2 = sensor.Sensor("Photodiode", ['in3/gnd'],3.3, 20)
 sensors = [Temperature1, Photodiode1, Photodiode2]
+
+def initADC():
+    ads1015 = ADS1015()
+    chip_type = ads1015.detect_chip_type()
+    print("Found: {}".format(chip_type))
+
+    ads1015.set_mode('single')
+    ads1015.set_programmable_gain(0.256)
+
+    if chip_type == 'ADS1015':
+        ads1015.set_sample_rate(1600)
+    else:
+        ads1015.set_sample_rate(860)
+
+    ADC = ads1015
+    return [ADC,ADC.get_reference_voltage()]
+
+def readADC(chip, inputPort):
+    v = chip[0].get_voltage(channel=inputPort[0])
+    return v
 
 def nMeasuresTimed(n, deltatms, SensorNr):
     #Start the timer
@@ -15,7 +46,7 @@ def nMeasuresTimed(n, deltatms, SensorNr):
 
     for i in range(n):
         #Measure
-        #sensors[SensorNr].readSensorValue(readADC(ADC, sensors[SensorNr].pin))
+        sensors[SensorNr].readSensorValue(readADC(ADC, sensors[SensorNr].pin))
         #Wait in the loop until time is elapsed
         while (True):
             tend = time.perf_counter_ns()
@@ -29,8 +60,15 @@ def nMeasuresTimed(n, deltatms, SensorNr):
         tstart = time.perf_counter_ns()
         #print("break", i)  #Test print to see how long it takes
     #Get the average
-    # value = sensors[SensorNr].getValue()
+    value = sensors[SensorNr].getValue()
     print("Total time:", time.perf_counter_ns()-ttot) #Test print for total
-    return 1
+    return value
 
-nMeasuresTimed(40, 1, 1)
+ADC = initADC()
+t1 = time.perf_counter_ns()
+sensors[1].readSensorValue(readADC(ADC, sensors[1].pin))
+t2 = time.perf_counter_ns()
+
+print("measure time:", t2-t1)
+
+nMeasuresTimed(20, 21, 1)
